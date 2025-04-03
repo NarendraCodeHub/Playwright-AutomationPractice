aa1655c5edb1303da79284bfbc0c0db83ec75c3d
Narendra122812 <narendrakumar122812@gmail.com>
2025-04-04 01:06:31 +0530

    Modify Code

--- tests/JavaScriptDelays.spec.js
+++ tests/JavaScriptDelays.spec.js
@@ -1,23 +1,16 @@
 import { test, expect } from '@playwright/test';
 
-test('Validate JavaScript Delays', async({page, baseURL}) => {
-
+test('Validate JavaScript Delays', async ({ page, baseURL }) => {
     await page.goto(baseURL);
 
-    await page.getByRole('link', { name: 'JavaScript Delays'}).click();
+    await page.getByRole('link', { name: 'JavaScript Delays' }).click();
 
     await page.waitForLoadState('domcontentloaded');
 
-    await expect(page.getByRole('heading', { name: 'JavaScript Delays'})).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'JavaScript Delays' })).toBeVisible();
 
     await page.locator('button#start').click();
 
-    await page.waitForFunction(() => {
-        const input = document.querySelector('#delay'); 
-        return input && input.value === 'Liftoff!';
-    }, { timeout: 30000 });
-
-    const countdownInput = page.locator('#delay');
-    await expect(countdownInput).toHaveValue('Liftoff!');
-
+    const countdownElement = page.locator('#delay');
+    await expect(countdownElement).toHaveText('Liftoff!', { timeout: 30000 });
 });
